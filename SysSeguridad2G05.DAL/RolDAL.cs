b1147ffee97c3f4f4f28315e2f5fa273d152192f
JLiz23 <jencyfranco682@gmail.com>
2026-03-18 11:25:58 -0600

    Se agrego la clase de UsuarioDAL

--- SysSeguridad2G05.DAL/RolDAL.cs
+++ SysSeguridad2G05.DAL/RolDAL.cs
@@ -11,10 +11,10 @@ namespace SysSeguridad2G05.DAL
         public static async Task<int> CrearAsync(Rol pRol)
         {
             int result = 0;
-            using (var dbContext = new DBContexto())
+            using (var dbContexto = new DBContexto())
             {
-                dbContext.Rol.Add(pRol);
-                result = await dbContext.SaveChangesAsync();
+                dbContexto.Rol.Add(pRol);
+                result = await dbContexto.SaveChangesAsync();
             }
             return result;
         }
@@ -22,12 +22,12 @@ namespace SysSeguridad2G05.DAL
         public static async Task<int> ModificarAsync(Rol pRol)
         {
             int result = 0;
-            using (var dbContext = new DBContexto())
+            using (var dbContexto = new DBContexto())
             {
-                var rol = await dbContext.Rol.FirstOrDefaultAsync(s => s.IdRol == pRol.IdRol);//Select
+                var rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.IdRol == pRol.IdRol);//Select
                 rol.Nombre = pRol.Nombre;
-                dbContext.Rol.Update(rol);
-                result = await dbContext.SaveChangesAsync();
+                dbContexto.Rol.Update(rol);
+                result = await dbContexto.SaveChangesAsync();
 
             }
             return result;
@@ -36,13 +36,54 @@ namespace SysSeguridad2G05.DAL
         public static async Task<int> EliminarAsync(Rol pIdRol)
         {
             int result = 0;
-            using (var dbContext = new DBContexto())
+            using (var dbContexto = new DBContexto())
             {
-                var rol = await dbContext.Rol.FirstOrDefaultAsync(s => s.IdRol == pIdRol.IdRol);
-                dbContext.Rol.Remove(rol);
-                result = await dbContext.SaveChangesAsync();
+                var rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.IdRol == pIdRol.IdRol);
+                dbContexto.Rol.Remove(rol);
+                result = await dbContexto.SaveChangesAsync();
             }
             return result;
+        }
+        public static async Task<Rol> ObtenerPorId(Rol pRol)
+        {
+            Rol rol = new Rol();
+            using (var dbContexto = new DBContexto())
+            {
+                rol = await dbContexto.Rol.FirstOrDefaultAsync(s => s.IdRol == pRol.IdRol);
+            }
+            return rol;
+        }
+        public static async Task<List<Rol>> ObtenerTodosAsync()
+        {
+            List<Rol> roles = new List<Rol>();
+            using (var dbContexto = new DBContexto())
+            {
+                roles = await dbContexto.Rol.ToListAsync();
+            }
+            return roles;
+        }
+
+        internal static IQueryable<Rol> QuerySelect(IQueryable<Rol> pQuery, Rol pRol)
+        {
+            if (pRol.IdRol > 0)
+                pQuery = pQuery.Where(s => s.IdRol == pRol.IdRol);
+            if (!string.IsNullOrWhiteSpace(pRol.Nombre))
+                pQuery = pQuery.Where(s => s.Nombre.Contains(pRol.Nombre));
+              pQuery = pQuery.OrderByDescending(s => s.IdRol).AsQueryable();
+            if(pRol.Top_Aux > 0)
+                pQuery = pQuery.Take(pRol.Top_Aux).AsQueryable();
+            return pQuery;
+        }
+        public static async Task<List<Rol>> BuscarAsync(Rol pRol)
+        {
+            var roles = new List<Rol>();
+            using (var dbContexto = new DBContexto())
+            {
+                var select = dbContexto.Rol.AsQueryable();
+                select = QuerySelect(select, pRol);
+                roles = await select.ToListAsync();
+            }
+            return roles;
         }
     }
 }
---
+++ SysSeguridad2G05.DAL/UsuarioDAL.cs
@@ -0,0 +1,101 @@
+﻿using SysSeguridad2G05.EN;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace SysSeguridad2G05.DAL
+{
+    public class UsuarioDAL
+    {
+        private static void EncriptarMD5(Usuario pUsuario)
+        {
+            using (var md5 = MD5.Create())
+            {
+                var result = md5.ComputeHash(Encoding.UTF8.GetBytes(
+                   pUsuario.Password));
+                var strEncriptar = "";
+                for (int i = 0; i < result.Length; i++)
+                    strEncriptar += result[i].ToString("x2").ToLower();
+                pUsuario.Password = strEncriptar;
+
+            }
+        }
+        private static async Task<bool> ExisteLogin(Usuario pUsuario, DBContexto pDContexto)
+        {
+            bool result = false;
+            var LoginUsuarioExiste = await pDContexto.Usuario.FirstOrDefaultAsync(u => u.Login == pUsuario.Login && u.IdUsuario != pUsuario.IdUsuario);
+            if (LoginUsuarioExiste != null && LoginUsuarioExiste.IdUsuario > 0 && LoginUsuarioExiste.Login == pUsuario.Login)
+                result = true;
+            {
+                return result;
+            }
+        }
+
+        #region "CRUD"
+
+        public static async Task<int> GuardarAsync(Usuario pUsuario)
+        {
+            int result = 0;
+            try
+            {
+                using (var DContexto = new DBContexto())
+                {
+
+                    bool existeLogin = await ExisteLogin(pUsuario, DContexto);
+                    if (existeLogin == false)
+                    {
+                        EncriptarMD5(pUsuario);
+                        DContexto.Add(pUsuario);
+                        result = await DContexto.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        throw new Exception("El Login ya existe");
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                result = 0;
+                throw new Exception(ex.Message);
+            }
+            return result;
+        }
+        public static async Task<int> ModificarAsync(Usuario pUsuario)
+        {
+            int result = 0;
+            try
+            {
+                using (var dbContexto = new DBContexto())
+                {
+                    bool existeLogin = await ExisteLogin(pUsuario, dbContexto);
+                    if (existeLogin == false)
+                    {
+                        var usuario = await dbContexto.Usuario.FirstOrDefaultAsync(u => u.IdUsuario == pUsuario.IdUsuario);
+                        usuario.IdRol = pUsuario.IdRol;
+                        usuario.Nombre = pUsuario.Nombre;
+                        usuario.Apellido = pUsuario.Apellido;
+                        usuario.Login = pUsuario.Login;
+                        usuario.status = pUsuario.status;
+                        dbContexto.Update(usuario);
+                        result = await dbContexto.SaveChangesAsync();
+                    }
+                    else
+                        throw new Exception("El Login ya existe");
+                }
+            }
+            catch (Exception ex)
+            {
+                result = 0;
+                throw new Exception(ex.Message);
+            }
+            return result;
+        }
+        #endregion
+    }
+}
+
