a3e1d2ad0fd8d27ae7422e441a85eac2945a8cd6
JLiz23 <jencyfranco682@gmail.com>
2026-03-18 08:06:31 -0600

    CLases de la DAL

---
+++ SysSeguridad2G05.DAL/DBContexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SysSeguridad2G05.EN;
+
+namespace SysSeguridad2G05.DAL
+{
+    public class DBContexto : DbContext
+    {
+       public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=JENCY\\SQLEXPRESS;Initial Catalog=DbSysSeguridad;Integrated Security=True;Trust Server Certificate=True");
+        }
+
+    }
+}
---
+++ SysSeguridad2G05.DAL/RolDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SysSeguridad2G05.EN;
+
+namespace SysSeguridad2G05.DAL
+{
+    public class RolDAL
+    {
+        public static async Task<int> CrearAsync(Rol pRol)
+        {
+            int result = 0;
+            using (var dbContext = new DBContexto())
+            {
+                dbContext.Rol.Add(pRol);
+                result = await dbContext.SaveChangesAsync();
+            }
+            return result;
+        }
+
+        public static async Task<int> ModificarAsync(Rol pRol)
+        {
+            int result = 0;
+            using (var dbContext = new DBContexto())
+            {
+                var rol = await dbContext.Rol.FirstOrDefaultAsync(s => s.IdRol == pRol.IdRol);//Select
+                rol.Nombre = pRol.Nombre;
+                dbContext.Rol.Update(rol);
+                result = await dbContext.SaveChangesAsync();
+
+            }
+            return result;
+        }
+
+        public static async Task<int> EliminarAsync(Rol pIdRol)
+        {
+            int result = 0;
+            using (var dbContext = new DBContexto())
+            {
+                var rol = await dbContext.Rol.FirstOrDefaultAsync(s => s.IdRol == pIdRol.IdRol);
+                dbContext.Rol.Remove(rol);
+                result = await dbContext.SaveChangesAsync();
+            }
+            return result;
+        }
+    }
+}
